5c3a9b22752aee98e5175ee97b55a0adf860d8a8
Nolan Harman <driazz106@gmail.com>
2020-09-14 17:14:39 -0600

    Back-end update

--- express-backend/public/scripts/buttonFunct.js
+++ express-backend/public/scripts/buttonFunct.js
@@ -19,40 +19,31 @@ $(document).ready(function () {
 });
 
 
-// Button Slide down toggle MAIN
-
-$(document).ready(function () {
-    const editButton = document.getElementById("buttonEdit");
-
-    $(".tablerow-adminEdit-td2").hide();
-    $(".whiteTop").hide();
-    $(".addButtonGlobal").click(function () {
-        if (editButton.innerHTML == "Edit Table") {
-            editButton.innerHTML = "Discard";
-            $("#buttonSubmit").addClass("submitButtonRev");
-            $(".addButtonGlobal").addClass("addPizzaButtonAct")
-        } else if (editButton.innerHTML == "Discard") {
-            editButton.innerHTML = "Edit Table";
-            $("#buttonSubmit").removeClass("submitButtonRev")
-            $(".addButtonGlobal").removeClass("addPizzaButtonAct");
-        }
-
-        $(".tablerow-adminEdit-td2").toggle();
-        $(".whiteTop").slideToggle("slow", function () {
-
-        });
-    });
-});
 
 
 
 
-//  Add pizza button
+
+
+
+// // Button Slide down toggle MAIN
+
 // $(document).ready(function () {
+//     const editButton = document.getElementById("buttonEdit");
+
 //     $(".tablerow-adminEdit-td2").hide();
 //     $(".whiteTop").hide();
+//     $(".addButtonGlobal").click(function () {
+//         if (editButton.innerHTML == "Edit Table") {
+//             editButton.innerHTML = "Discard";
+//             $("#buttonSubmit").addClass("submitButtonRev");
+//             $(".addButtonGlobal").addClass("addPizzaButtonAct")
+//         } else if (editButton.innerHTML == "Discard") {
+//             editButton.innerHTML = "Edit Table";
+//             $("#buttonSubmit").removeClass("submitButtonRev")
+//             $(".addButtonGlobal").removeClass("addPizzaButtonAct");
+//         }
 
-//     $(".addPizzaButton").click(function () {
 //         $(".tablerow-adminEdit-td2").toggle();
 //         $(".whiteTop").slideToggle("slow", function () {
 
@@ -60,146 +51,96 @@ $(document).ready(function () {
 //     });
 // });
 
-
-
-
-//Button Change UPDATED CODE
+// //Sauces Button
 // $(document).ready(function () {
-//     var editButton = document.getElementById("buttonEdit");
-//     var submitButton = document.getElementById("buttonSubmit");
+//     $(".saucesContainer").hide();
+//     $(".sauceButton").click(function () {
+//         $(".saucesContainer").slideToggle(function () {
 
-//     $(".addPizzaButton").click(function () {
-//         if (editButton.innerHTML == "Edit Table") {
-//             editButton.innerHTML = "Discard";
-//             $(".submitButton").addClass("submitButtonRev");
-//             $(".addPizzaButton").addClass("addPizzaButtonAct");
-
-//         } else if (editButton.innerHTML == "Discard") {
-//             editButton.innerHTML = "Edit Table";
-//             $(".submitButton").removeClass("submitButtonRev");
-//             $(".addPizzaButton").removeClass("addPizzaButtonAct");
-//         }
+//         });
 //     });
 // });
 
-//Button Change
-// function changeButton() {
-//     var button = document.getElementById("buttonEdit");
-//     var submitButton = document.getElementById("submitButtonC");
 
-//     if (button.innerHTML == "Edit Table") {
-//         button.innerHTML = "Discard"
-//         button.style.backgroundColor = "red";
-//         submitButton.innerHTML = "<button type='submit' class='submitButton'>Submit</button>";
-
-
-//     } else if (button.innerHTML = "Discard") {
-//         button.innerHTML = "Edit Table";
-//         button.style.backgroundColor = "#45BDEE";
-//         submitButton.innerHTML = "";
-
-//         $(".submitButton").removeClass("submitButtonRev");
-//     }
-// }
-
-//Sauces Button
-$(document).ready(function () {
-    $(".saucesContainer").hide();
-    $(".sauceButton").click(function () {
-        $(".saucesContainer").slideToggle(function () {
-
-        });
-    });
-});
-
-
-//Animation button for sauces
-$(document).ready(function () {
-    $(".sauceButton").hover(function () {
-        $(this).css("box-shadow", "0px 0px 6px 1px gold");
-        $(this).css("color", "blue");
-    }, function () {
-        $(this).css("box-shadow", "none");
-        $(this).css("color", "white");
-    });
-});
-
-
-//Meats Button
-$(document).ready(function () {
-    $(".meatsContainer").hide();
-    $(".meatButton").click(function () {
-        $(".meatsContainer").slideToggle(function () {
-
-        });
-    });
-});
-
-//Animation button for meats
-$(document).ready(function () {
-    $(".meatButton").hover(function () {
-        $(this).css("box-shadow", "0px 0px 6px 1px gold");
-        $(this).css("color", "blue");
-    }, function () {
-        $(this).css("box-shadow", "none");
-        $(this).css("color", "white");
-    });
-})
-
-//Veg Button
-$(document).ready(function () {
-    $(".vegContainer").hide();
-    $(".vegButton").click(function () {
-        $(".vegContainer").slideToggle(function () {
-
-        });
-    });
-});
-
-//Veg Animation
-$(document).ready(function () {
-    $(".vegButton").hover(function () {
-        $(this).css("box-shadow", "0px 0px 6px 1px gold");
-        $(this).css("color", "blue");
-    }, function () {
-        $(this).css("box-shadow", "none");
-        $(this).css("color", "white");
-    });
-})
-
-//Cheese Button
-$(document).ready(function () {
-    $(".cheeseContainer").hide();
-    $(".cheeseButton").click(function () {
-        $(".cheeseContainer").slideToggle(function () {
-
-        });
-    });
-});
-
-$(document).ready(function () {
-    $(".cheeseButton").hover(function () {
-        $(this).css("box-shadow", "0px 0px 6px 1px gold");
-        $(this).css("color", "blue");
-    }, function () {
-        $(this).css("box-shadow", "none");
-        $(this).css("color", "white");
-    });
-});
-
-//Info Button- EXPAND
-
-$(document).ready(function () {
-    var getDataClass = $(".pizza-ID-data").html();
-    getDataClass = toString(getDataClass);
-
-
-});
-
-// Button Controls Edit and Remove
+// //Animation button for sauces
 // $(document).ready(function () {
-//     $(".pizzaData-td").hide();
-//     $(".").hover(function () {
-//         $(".pizzaData-td").toggle();
+//     $(".sauceButton").hover(function () {
+//         $(this).css("box-shadow", "0px 0px 6px 1px gold");
+//         $(this).css("color", "blue");
+//     }, function () {
+//         $(this).css("box-shadow", "none");
+//         $(this).css("color", "white");
 //     });
+// });
+
+
+// //Meats Button
+// $(document).ready(function () {
+//     $(".meatsContainer").hide();
+//     $(".meatButton").click(function () {
+//         $(".meatsContainer").slideToggle(function () {
+
+//         });
+//     });
+// });
+
+// //Animation button for meats
+// $(document).ready(function () {
+//     $(".meatButton").hover(function () {
+//         $(this).css("box-shadow", "0px 0px 6px 1px gold");
+//         $(this).css("color", "blue");
+//     }, function () {
+//         $(this).css("box-shadow", "none");
+//         $(this).css("color", "white");
+//     });
+// })
+
+// //Veg Button
+// $(document).ready(function () {
+//     $(".vegContainer").hide();
+//     $(".vegButton").click(function () {
+//         $(".vegContainer").slideToggle(function () {
+
+//         });
+//     });
+// });
+
+// //Veg Animation
+// $(document).ready(function () {
+//     $(".vegButton").hover(function () {
+//         $(this).css("box-shadow", "0px 0px 6px 1px gold");
+//         $(this).css("color", "blue");
+//     }, function () {
+//         $(this).css("box-shadow", "none");
+//         $(this).css("color", "white");
+//     });
+// })
+
+// //Cheese Button
+// $(document).ready(function () {
+//     $(".cheeseContainer").hide();
+//     $(".cheeseButton").click(function () {
+//         $(".cheeseContainer").slideToggle(function () {
+
+//         });
+//     });
+// });
+
+// $(document).ready(function () {
+//     $(".cheeseButton").hover(function () {
+//         $(this).css("box-shadow", "0px 0px 6px 1px gold");
+//         $(this).css("color", "blue");
+//     }, function () {
+//         $(this).css("box-shadow", "none");
+//         $(this).css("color", "white");
+//     });
+// });
+
+// //Info Button- EXPAND
+
+// $(document).ready(function () {
+//     var getDataClass = $(".pizza-ID-data").html();
+//     getDataClass = toString(getDataClass);
+
+
 // });
